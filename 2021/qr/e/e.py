66f6ef89992b951c971e9e98bf6b186545c20251
Manuel García García <marianocucufate@gmail.com>
2021-03-26 16:06:50 +0100

    added 2021 QR

---
+++ 2021/qr/e/e.py
@@ -0,0 +1,35 @@
+# coding=utf-8
+
+# Code Jam 2021
+# Round QR
+# Problem E
+# @author: manolo
+
+from sys import stdin, stdout
+
+
+def read_line():
+    return stdin.readline()[:-1]
+
+
+def read_one_int_line():
+    return int(read_line()[:-1])
+
+
+def read_int_line():
+    return [int(x[:-1]) for x in read_line().split(' ')]
+
+
+def write(case, what):
+    stdout.write('Case #{}: {}\n'.format(case, what))
+
+
+def solve(e, r, n):
+    return 0
+
+
+T = read_one_int_line()
+for case in range(1, T + 1):
+    e, r, n = read_int_line()
+    what = solve(e, r, n)
+    write(case, what)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# code-jam
+# Code Jam
+
+Use `gen` script to generate all files for the Qualification Round for the current year
